a88ecef0fad02a5868c202c9a0968a57db65835e
agung <agung@drosha.com>
2022-03-21 07:18:37 +0700

    chap 97

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Ordering.Application.Exceptions
+{
+    [Serializable]
+    internal class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
